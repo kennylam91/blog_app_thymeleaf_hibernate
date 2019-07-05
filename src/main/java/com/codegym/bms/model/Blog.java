fe1b19578edbcbe6ff99e5115c3ec4e4fbff3766
kennylam91 <phamvanlam1991@gmail.com>
2019-07-05 11:59:07 +0700

    create model and table OK

---
+++ src/main/java/com/codegym/bms/model/Blog.java
@@ -0,0 +1,56 @@
+package com.codegym.bms.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "blogs")
+public class Blog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
+    private Long id;
+    private String name;
+    private String content;
+    private String category;
+
+    public Blog() {
+    }
+
+    public Blog(String name, String content, String category) {
+        this.name = name;
+        this.content = content;
+        this.category = category;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
